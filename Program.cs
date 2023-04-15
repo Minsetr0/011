9e18bcccbe0d379f0fe33380593c88fe7a0ddbe2
Minsetr0 <nesterovmaksim71@mail.ru>
2023-04-15 12:43:43 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
-﻿string bracketsString = "((())())";
+﻿string bracketsString = "())(()";
 int nestingDepth = 0;
 int validation = 0;
-int maxNestingDepth = 0;
 int checkValidation = 0;
+int maxNestingDepth = 0;
 
 for (int i = 0; i < bracketsString.Length; i++)
 {
@@ -21,6 +21,11 @@ for (int i = 0; i < bracketsString.Length; i++)
         validation--;
         nestingDepth--;
     }
+
+    if (validation < checkValidation)
+    {
+        break;
+    }
 }
 
 Console.WriteLine(bracketsString);
@@ -29,7 +34,7 @@ if (validation == checkValidation)
 {
     Console.WriteLine("Данная строка символов является корректным скобочным выражением, а так же её максимальная глубина равна " + maxNestingDepth);
 }
-else if (validation > checkValidation)
+else if (validation != checkValidation)
 {
     Console.WriteLine("Данная строка символов является некорректным скобочным выражением");
 }
